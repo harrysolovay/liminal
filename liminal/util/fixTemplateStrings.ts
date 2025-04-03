b3e1e5589f706816123c517dbd6b5e725cab0aa3
Cole Lawrence <cole@colelawrence.com>
2025-04-02 23:05:42 -0300

    fixup trailing spaces
    Signed-off-by: Cole Lawrence <cole@colelawrence.com>

--- liminal/util/fixTemplateStrings.ts
+++ liminal/util/fixTemplateStrings.ts
@@ -2,13 +2,17 @@ const INDENTATION_RE = /^\n([ \t]+)/
 export function fixTemplateStrings(template: TemplateStringsArray): { readonly raw: readonly string[] } {
   const indentation = INDENTATION_RE.exec(template.raw[0]!)?.[1]
   return {
-    raw: template.raw.map((a) =>
-      (indentation ? a.replaceAll(`\n${indentation}`, "\n") : a)
+    raw: template.raw.map((a, i, arr) => {
+      let res = (indentation ? a.replaceAll(`\n${indentation}`, "\n") : a)
         // escaped next lines
-        .replace("\\\n", "")
+        .replaceAll("\\\n", "")
+        // fix weird things escaped in tag calls
         .replaceAll("\\`", "`")
         .replaceAll("\\$", "$")
         .replaceAll("\\{", "{")
-    ),
+      // fix trailing spaces
+      if (indentation && i === arr.length - 1) res = res.replace(/\n[ \t]+$/, "")
+      return res
+    }),
   }
 }
