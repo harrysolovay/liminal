0fc6393ebc9a2439f46b84b5e0361cd3566df0cc
Harry Solovay <harrysolovay@gmail.com>
2025-04-03 15:36:04 -0400

    Fix Exports / Namespacing

--- liminal/index.ts
+++ liminal/index.ts
@@ -1,16 +1,48 @@
+export * from "./Action.ts"
+export * from "./ActionEvent.ts"
+export * from "./ActionEvents.ts"
+export type { EnteredEvent, ExitedEvent } from "./actions/actions_base.ts"
+export type { Arg } from "./actions/Arg.ts"
+export * from "./actions/content_part.ts"
+export type { Context, ContextEvent } from "./actions/Context.ts"
+export type { DisableTool, ToolDisabledEvent } from "./actions/DisableTool.ts"
+export type { Embed, EmbeddedEvent, EmbeddingRequestedEvent } from "./actions/Embed.ts"
+export type { Emit, EmittedEvent } from "./actions/Emit.ts"
+export type {
+  EnableTool,
+  ToolCalledEvent,
+  ToolEnabledEvent,
+  ToolEvent,
+  ToolImplementation,
+  ToolResult,
+} from "./actions/EnableTool.ts"
+export type { Fork, ForkArmEvent, ForkArms, ForkEvent, ForkResult } from "./actions/Fork.ts"
+export type { GetScope } from "./actions/GetScope.ts"
+export type { Infer, InferenceRequestedEvent, InferredEvent } from "./actions/Infer.ts"
+export type {
+  AssistantContent,
+  AssistantMessage,
+  AssistantMessagedEvent,
+  Message,
+  SystemMessage,
+  SystemMessagedEvent,
+  ToolContentPart,
+  ToolMessage,
+  ToolMessagedEvent,
+  UserContent,
+  UserMessage,
+  UserMessagedEvent,
+} from "./actions/messages.ts"
+export type { EmbeddingModelSetEvent, RunEmbed, SetEmbeddingModel } from "./actions/SetEmbeddingModel.ts"
+export type { LanguageModelSetEvent, RunInfer, SetLanguageModel } from "./actions/SetLanguageModel.ts"
 export * from "./Actor/ActionLike.ts"
 export * from "./Actor/Actor.ts"
 export * from "./Actor/ActorLike.ts"
 export * from "./Actor/reduce.ts"
-export * from "./exec.ts"
+export { exec, type ExecConfig } from "./exec.ts"
 export * as L from "./L.ts"
-export * from "./LiminalAssertionError.ts"
+export { LiminalAssertionError } from "./LiminalAssertionError.ts"
 export * from "./Scope.ts"
 export * from "./Spec.ts"
-export * from "./types/intrinsics.ts"
-export * from "./types/JSONRootType.ts"
-export * from "./types/JSONType.ts"
-export * from "./types/RecursiveTypeVisitorState.ts"
-export * from "./types/TypeVisitor.ts"
-export * from "./util/JSONValue.ts"
+export * from "./types/non_factories.ts"
 export * as _util from "./util/util.ts"
---
+++ liminal/types/index.ts
@@ -0,0 +1,2 @@
+export * as L from "./intrinsics.ts"
+export * from "./non_factories.ts"
