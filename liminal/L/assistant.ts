8acdc27c47cc36572272e103cd06c03d9c0b5d45
Harry Solovay <harrysolovay@gmail.com>
2025-04-20 21:02:12 -0400

    blah

--- liminal/L/assistant.ts
+++ liminal/L/assistant.ts
@@ -1,5 +1,5 @@
 import type { Rune } from "../Rune.ts"
-import type { SchemaRoot } from "../Schema1/SchemaRoot.ts"
+import type { SchemaRoot } from "../Schema/SchemaRoot.ts"
 import { LiminalAssertionError } from "../util/LiminalAssertionError.ts"
 import { _inference } from "./_inference.ts"
 import { _message } from "./_message.ts"
--- liminal/Model.ts
+++ liminal/Model.ts
@@ -1,5 +1,5 @@
 import type { Message } from "./Message.ts"
-import type { Schema } from "./Schema1/Schema.ts"
+import type { Schema } from "./Schema/Schema.ts"
 
 export interface Model {
   resolve(messages: Array<Message>, schema?: Schema): Promise<string>
