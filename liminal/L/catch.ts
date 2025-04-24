14f870a5b0e0ed833a1635f4bc496b0b7f04e813
Harry Solovay <harrysolovay@gmail.com>
2025-04-24 19:40:50 -0400

    Reintroduce `L.catch` (#271)

---
+++ liminal/L/catch.ts
@@ -0,0 +1,24 @@
+import { Fiber } from "../Fiber.ts"
+import type { Rune } from "../Rune.ts"
+import type { Runic } from "../Runic.ts"
+import { rune } from "./rune.ts"
+
+export { catch_ as catch }
+function* catch_<Y extends Rune, T>(runic: Runic<Y, T>): Generator<Rune<Y>, CatchResult<T>> {
+  return yield* rune(async () => {
+    try {
+      return { resolved: await new Fiber(runic).resolution() }
+    } catch (exception: unknown) {
+      return { rejected: exception }
+    }
+  })
+}
+Object.defineProperty(catch_, "name", { value: "catch" })
+
+export type CatchResult<T> = {
+  resolved: T
+  rejected?: never
+} | {
+  resolved?: never
+  rejected: unknown
+}
