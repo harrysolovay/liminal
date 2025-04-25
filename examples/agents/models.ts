ae33814e070623d970968c65dd994a0c18cf8a88
Harry Solovay <harrysolovay@gmail.com>
2025-04-25 10:09:50 -0400

    add explicit type signatures in models file

--- examples/agents/models.ts
+++ examples/agents/models.ts
@@ -1,13 +1,14 @@
 import { openai } from "@ai-sdk/openai"
+import type { Model } from "liminal"
 import { ai } from "liminal-ai"
 import { ollama } from "liminal-ollama"
 
-export const gpt4o = ai(openai("gpt-4o-mini", {
+export const gpt4o: Model = ai(openai("gpt-4o-mini", {
   structuredOutputs: true,
 }))
-export const gpt4oMini = ai(openai("gpt-4o-mini", {
+export const gpt4oMini: Model = ai(openai("gpt-4o-mini", {
   structuredOutputs: true,
 }))
-export const o1Mini = ai(openai("o1-mini"))
-export const gemma3 = ollama("gemma3:1b")
-export const mistralSmall31 = ollama("mistral-small3.1")
+export const o1Mini: Model = ai(openai("o1-mini"))
+export const gemma3: Model = ollama("gemma3:1b")
+export const mistralSmall31: Model = ollama("mistral-small3.1")
