b232a3aee471110fc44bc2be3bd830bcb10ea621
Harry Solovay <harrysolovay@gmail.com>
2025-08-23 20:00:02 -0400

    clean up raw util

--- liminal/util/raw.ts
+++ liminal/util/raw.ts
@@ -1,26 +1,27 @@
 import * as Effect from "effect/Effect"
+import type { YieldWrap } from "effect/Utils"
 import type { ExtractE, ExtractR } from "./extract.ts"
 
 export type TemplateStringsArrayLike = {
   raw: readonly string[] | ArrayLike<string>
 }
 
-export const raw: <Substitutions extends Array<unknown>>(
+export const raw = Effect.fnUntraced(function*<Substitutions extends Array<unknown>>(
   template: TemplateStringsArrayLike,
   ...substitutions: Substitutions
-) => Effect.Effect<
+): Generator<
+  YieldWrap<
+    Effect.Effect<Array<string>, ExtractE<Substitutions[number]>, ExtractR<Substitutions[number]>>
+  >,
   string,
-  ExtractE<Substitutions[number]>,
-  ExtractR<Substitutions[number]>
-> = Effect.fnUntraced(function*(template, ...substitutions) {
+  never
+> {
   return String.raw(
     template,
-    ...yield* (Effect.all(
-      substitutions.map((v) =>
-        Effect.isEffect(v)
-          ? v
-          : Effect.succeed(v)
-      ),
-    ) as never as Effect.Effect<Array<unknown>>),
+    ...yield* (Effect.all(substitutions.map((v) => Effect.isEffect(v) ? v : Effect.succeed(v))) as Effect.Effect<
+      Array<string>,
+      ExtractE<Substitutions[number]>,
+      ExtractR<Substitutions[number]>
+    >),
   )
 })
